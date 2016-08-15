14fdc4e15012a66e74374b688e43d337137bc0c6
bau192 <bau15192@uvg.edu.gt>
2016-08-15 17:24:40 -0600

    actualizacion del factory

--- Factory.java
+++ Factory.java
@@ -1,22 +1,55 @@
+//##################################
+//Universiad del Valle de Guatemala
+//Algorirmos y Estructura de Datos
+//Erick Bautista 15192
+//Brandon Hernandez 15326
+//##################################
+
 import java.util.ArrayList;
 public class Factory <E> {
-	public Object tipoLista(String implementacionDeStack, String metodoDeStack, Object instrucciones){
+	public Object tipoLista(String implementacionDeStack, String metodoDeStack, String instrucciones){
 		
-		if(implementacionDeStack.equals("1")){
-			ArrayList lista = new ArrayList();
+		//condicion para crear arrayList de instrucciones
+		if(implementacionDeStack.equals("1"))
+		{
+			ArrayList listaAL = new ArrayList();
+			
+			
+			for(int posicion=0;posicion<intrucciones.length();posicion++)
+			{
+				//nuestro array es de string
+				String caracter=intrucciones.substring(posicion,posicion+1);
+				listaAL.add(posicion,caracter);
+			}	
+			
 			
 		}
-		if(implementacionDeStack.equals("2")){
+		//condicion para crear vector de instrucciones
+		if(implementacionDeStack.equals("2"))
+		{
 			
+			String [] listaS=new [intrucciones.length()] String();
+			
+			for(int posicion=0;posicion<intrucciones.length();posicion++)
+			{
+				String caracter=intrucciones.substring(posicion,posicion+1);
+				listaS[posicion]=caracter;				
+			}	
 		}
-		if(implementacionDeStack.equals("3")){
-			if(metodoDeStack.equals("1")){
+		
+		//condicion para crear lista de istrucciones
+		if(implementacionDeStack.equals("3"))
+		{
+			if(metodoDeStack.equals("1"))
+			{
 				
 			}
-			if(metodoDeStack.equals("2")){
+			if(metodoDeStack.equals("2"))
+			{
 				
 			}
-			if(metodoDeStack.equals("3")){
+			if(metodoDeStack.equals("3"))
+			{
 				
 			}
 		}
