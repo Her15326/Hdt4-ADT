4c26a7803523195afbc04f5f46a857abef2ab1dc
bau192 <bau15192@uvg.edu.gt>
2016-08-15 17:45:55 -0600

    actualizacion

--- I_Calculadora.java
+++ I_Calculadora.java
@@ -1,13 +1,14 @@
-/**
-*I_Calculadora, Esta define los metodos que seran implementados en la calculadora
-*@version: 3.0
-*@author: Robbin Woods 15201, Steven Rubio 15044, Erick Bautista 15192
-*@since 2016-07-24
-*/
+//##################################
+//Universiad del Valle de Guatemala
+//Algorirmos y Estructura de Datos
+//Erick Bautista 15192
+//Brandon Hernandez 15326
+//##################################
 
 
 public interface I_Calculadora{
-	public int calcular(String vector);
+	
+	public int calcularVector(object);
 	// pre: recibe el string con las intrucciones
 	// post: regresa el resultado de las operacaciones
 	public String toString();
@@ -15,4 +16,5 @@ public interface I_Calculadora{
 	public String leerArchivo(String direccion);
 	// pre:	Carga en un String la direccion de un Archivo
 	// post: retorna en un String la operacion a realizar
-}
+
+	}
